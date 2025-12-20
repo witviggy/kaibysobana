4a0807c2861c4ca2ac846b363135ec23fcde0d1e
Deepan <deepksami@gmail.com>
2025-12-21 03:18:49 +0530

    Replaced wget with curl in keycloak

--- keycloak/keycloak-entrypoint.sh
+++ keycloak/keycloak-entrypoint.sh
@@ -1,36 +1,41 @@
 #!/bin/sh
-# Fully hard-coded Keycloak startup script
+set -e
 
-# Start Keycloak in dev mode with realm import
+echo "Starting Keycloak..."
+
+# Start Keycloak
 /opt/keycloak/bin/kc.sh start-dev --import-realm &
 
-# Wait for Keycloak to be ready
-until wget -q --spider https://kaibysobana-keycloak.purpleisland-0b71ed79.centralindia.azurecontainerapps.io/realms/master; do
+# Wait until Keycloak is ready (internal HTTP)
+until curl -sf http://localhost:8080/realms/master; do
+  echo "Waiting for Keycloak..."
   sleep 5
 done
 
-# Set credentials
+echo "Keycloak is up. Configuring..."
+
+# Authenticate CLI
 /opt/keycloak/bin/kc.sh config credentials \
-  --server https://kaibysobana-keycloak.purpleisland-0b71ed79.centralindia.azurecontainerapps.io \
+  --server http://localhost:8080 \
   --realm master \
   --user admin \
   --password admin
 
-# Create client (ignore if exists)
 CLIENT_ID=kai-frontend
 REALM=kai
 
+# Create client (idempotent)
 /opt/keycloak/bin/kc.sh create clients -r $REALM \
   -s clientId=$CLIENT_ID \
   -s publicClient=true \
-  -s directAccessGrantsEnabled=true || true
+  -s standardFlowEnabled=true \
+  -s directAccessGrantsEnabled=false || true
 
-# Update Web Origins & Redirect URIs for CORS / iframe
+# Update frontend config
 /opt/keycloak/bin/kc.sh update clients/$CLIENT_ID -r $REALM \
   -s webOrigins="[\"https://kaibysobana-frontend.purpleisland-0b71ed79.centralindia.azurecontainerapps.io\"]" \
   -s redirectUris="[\"https://kaibysobana-frontend.purpleisland-0b71ed79.centralindia.azurecontainerapps.io/*\"]"
 
-echo "Keycloak client configured for frontend."
+echo "Keycloak client configured."
 
-# Keep container running
 wait
